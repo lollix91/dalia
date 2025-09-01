5fd585fdd4a97e94100aab1dd923fa6536e58410
alyshmahell <aly.shmahell@gmail.com>
2025-09-01 15:55:47 +0200

    got things running

--- startmas.py
+++ startmas.py
@@ -1,15 +1,18 @@
 import os 
 import git
-import glob
+import time
 import json
 import logging
 import asyncio
+import argparse
+import threading
 import subprocess
+logging.basicConfig(level=logging.INFO)
 
-def check_port(port):
+def port_busy(port):
     result = subprocess.run(['netstat', '-an'], capture_output=True, text=True)
     logging.info(result.stdout)
-    return f"{port}" not in result.stdout
+    return f"{port}" in result.stdout
 
 def check_sicstus(root):
     sicstus = ""
@@ -63,12 +66,20 @@ def rmdir(path):
                 os.rmdir(os.path.join(root, dir))
         os.rmdir(path)
 
-def build(root):
+def shell(cmd):
+    p = subprocess.Popen(cmd, shell=True)
+    p.wait()
+
+
+def build(*, root, sicstuspath, dalipath):
     src = load_src(root)
     if not src:
         return
-    dalipath = os.path.join(root, 'files', 'dali', 'src')
-    sicstus  = os.path.join(root, 'files', 'sicstus', 'bin', 'sicstus')
+    
+    cmd = "pkill -9 sicstus 2>/dev/null || true"
+    shell(cmd)
+    dalipath = os.path.join(dalipath, 'src')
+    sicstus  = os.path.join(sicstuspath, 'bin', 'sicstus')
     wrk = os.path.join(root, 'wrk')
     rmdir(wrk)
     agentsdir = os.path.join(wrk, 'agents')
@@ -79,7 +90,15 @@ def build(root):
     os.makedirs(confdir, exist_ok=True)
     with open(os.path.join(confdir, f'communication.con'), 'w') as f:
         f.write(src["comm"])
-    procs = []
+    port = 3010
+    logging.info(f"waiting for port {port} to be free")
+    while port_busy(port):
+        time.sleep(1)
+    time.sleep(5)
+    cmd = f"{sicstus} --noinfo -l {os.path.join(dalipath, 'active_server_wi.pl')} --goal go."
+    logging.info(cmd)
+    threading.Thread(target=shell, args=(cmd,), daemon=True).start()
+    time.sleep(5)
     for name in src["agents"]:
         with open(os.path.join(agentsdir, f'{name}.txt'), 'w') as f:
             f.write(src['agents'][name]["code"])
@@ -88,13 +107,20 @@ def build(root):
         with open(os.path.join(setupsdir, f'{name}.txt'), 'w') as f:
             f.write(setup)
         os.chmod(os.path.join(setupsdir, f'{name}.txt'), 0o755)
-        procs +=[
-            subprocess.Popen([sicstus, '--noinfo', '-l', os.path.join(dalipath, 'active_dali_wi.pl'), '--goal', f"\"start0('{os.path.join(setupsdir, f'{name}.txt')}').\""])
-        ]
-        
-    for p in procs:
-        p.wait()
-    
+        # /opt/sicstus/bin/sicstus --noinfo -l ../../src/active_dali_wi.pl --goal "start0('/home/aly/study/DALI/Examples/advanced/conf/mas/agent1.txt')."
+        cmd = " ".join([sicstus, '--noinfo', '-l', os.path.join(dalipath, 'active_dali_wi.pl'), '--goal', f'"start0(\'{os.path.join(setupsdir, f'{name}.txt')}\')."'])
+        logging.info(f"cmd: {cmd}")
+        threading.Thread(target=shell, args=(cmd,), daemon=True).start()
+        time.sleep(5)
+    cmd=f"{sicstus} --noinfo -l {os.path.join(dalipath, 'active_user_wi.pl')} --goal user_interface."
+    shell(cmd)
 
-root = os.path.dirname(os.path.abspath(__file__))
-build(root)
+
+    
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--dalipath', type=str, required=True)
+    argparser.add_argument('--sicstuspath', type=str, required=True)
+    args = argparser.parse_args()
+    root = os.path.dirname(os.path.abspath(__file__))
+    build(root=root, sicstuspath=os.path.abspath(args.sicstuspath), dalipath=os.path.abspath(args.dalipath))
