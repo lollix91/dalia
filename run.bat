3ded2da08de71b8e00edda2cad6788373042e094
lorenzo <lollix@live.it>
2025-10-21 16:05:05 +0200

    now, when quitting the app with CTRL+C, and starting it again, there is no need to build again the image, thus it is faster

--- run.bat
+++ run.bat
@@ -1,17 +1,13 @@
 @echo off
 setlocal
 
-REM Inizializza le variabili
 set "DALI="
 set "SRC="
 
-REM Parsing degli argomenti in stile loop
 :parse_args
 if "%~1"=="" goto end_parse_args
 
 if /I "%~1"=="--src" (
-    rem Converte il percorso relativo/parziale (%~2) in un percorso assoluto
-    rem Questo è l'equivalente batch di (cd "$2" && pwd)
     for %%F in ("%~2") do set "SRC=%%~fF"
     shift
 ) else if /I "%~1"=="--dali" (
@@ -29,25 +25,22 @@ goto :parse_args
 echo "SRC: %SRC%"
 echo "DALI: %DALI%"
 
-REM Controlla se Docker è in esecuzione
 docker info > nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Docker non è in esecuzione. Avvia Docker Desktop e attendi che sia pronto.
     exit /b 1
 )
 
-REM Pulisce Docker
-docker system prune -f
-docker container prune -f
-docker image prune -f
+docker container prune -f 
 
-REM Imposta le variabili d'ambiente che docker-compose leggerà
 set "src=%SRC%"
 set "dali=%DALI%"
 
-REM Esegui i comandi di docker-compose
+echo Avvio dei container...
 docker compose down --remove-orphans
 docker compose up -d --build --force-recreate --no-deps
+
+echo Visualizzazione dei log...
 docker compose logs -f
 
 endlocal
